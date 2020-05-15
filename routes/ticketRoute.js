5f21839bcfeec033fd278f483c4c2fed9d14acda
aruki <Joeld1027@gmail.com>
2020-05-14 23:20:42 -0500

    added more delete route

--- routes/ticketRoute.js
+++ routes/ticketRoute.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res, next) => {
 		if (foundTickets) {
 			return res.status(200).json(foundTickets);
 		}
-		return res.status(404).send('No Tickets found');
+		return res.status(404).json({ message: 'No Tickets found' });
 	} catch (err) {
 		return next(err);
 	}
@@ -25,6 +25,58 @@ router.post('/', async (req, res, next) => {
 		return res.status(200).json(newTicket);
 	} catch (err) {
 		return next(err);
+	}
+});
+
+router.get('/:ticketId', async (req, res, next) => {
+	try {
+		const foundTicket = await db.Ticket.findById(req.params.ticketId);
+		if (foundTicket) {
+			return res.status(200).json(foundTicket);
+		}
+		return res.status(404).json({
+			message: 'No ticket found',
+		});
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.put('/:ticketId', async (req, res, next) => {
+	try {
+		const updatedTicket = await db.Ticket.findOneAndUpdate(
+			{ _id: req.params.ticketId },
+			req.body,
+			{ new: true }
+		);
+		if (updatedTicket) {
+			return res.status(200).json(updatedTicket);
+		}
+		return res.status(404).json({
+			message: 'Ticket not found',
+		});
+	} catch (err) {
+		return next(err);
+	}
+});
+
+router.delete('/:ticketId', async (req, res, next) => {
+	try {
+		const deletedTicket = await db.Ticket.findByIdAndDelete({
+			_id: req.params.ticketId,
+		});
+		if (deletedTicket) {
+			return res.json({
+				message: 'Ticket deleted',
+				ok: true,
+			});
+		}
+		return next();
+	} catch (err) {
+		return next({
+			message: err.message,
+			ok: false,
+		});
 	}
 });
 
