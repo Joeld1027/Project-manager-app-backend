a16923d44ecc2082d903301649165da8eefdf8c0
aruki <Joeld1027@gmail.com>
2021-01-25 18:26:48 -0600

    allowed CORS

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('./handlers/error');
-const { loginRequired } = require('./middleware/auth');
+const {
+	loginRequired
+} = require('./middleware/auth');
 
 // Routes
 const authRoutes = require('./routes/authRoute');
@@ -17,6 +19,20 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(allowCrossDomain);
+
+var allowCrossDomain = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+	// intercept OPTIONS method
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	} else {
+		next();
+	}
+};
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
